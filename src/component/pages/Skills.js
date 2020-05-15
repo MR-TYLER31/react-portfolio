b09c5e493e9b973d85f47056ebc3ff9f5334e2fb
Tyler Snyder <tylersnyder619406@gmail.com>
2020-05-15 13:09:45 -0600

    added animations on hovered skills

--- src/component/pages/Skills.js
+++ src/component/pages/Skills.js
@@ -12,28 +12,48 @@ function Skills() {
       <div className="container">
         <div className="row mt-5">
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fab fa-html5 fa-5x"></i>
                 <p className="text-white">HTML5</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">HTML Images</h5>
+                  <h5 className="text-white text-center">Forms</h5>
+                </div>
               </div>
             </div>
           </div>
-
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fab fa-css3-alt fa-5x"></i>
                 <p className="text-white">CSS3</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">Bootstrap</h5>
+                  <h5 className="text-white text-center">Materialize</h5>
+                  <h5 className="text-white text-center">Bulma</h5>
+                  <h5 className="text-white text-center">Material UI</h5>
+                  <h5 className="text-white text-center">FlexBox</h5>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fab fa-js fa-5x"></i>
                 <p className="text-white">JavaScript</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">JQuery</h5>
+                  <h5 className="text-white text-center">Web APIs</h5>
+                  <h5 className="text-white text-center">AJAX</h5>
+                  <h5 className="text-white text-center">Axios</h5>
+                  <h5 className="text-white text-center">ES6</h5>
+                </div>
               </div>
             </div>
           </div>
@@ -41,28 +61,50 @@ function Skills() {
 
         <div className="row mt-5">
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fab fa-node fa-5x"></i>
-                <p className="text-white">Node JS</p>
+                <p className="text-white">Node.js</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">Express</h5>
+                  <h5 className="text-white text-center">MVC</h5>
+                  <h5 className="text-white text-center">NPM</h5>
+                  <h5 className="text-white text-center">Passport.js</h5>
+                  <h5 className="text-white text-center">Handlebars</h5>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fas fa-database  fa-5x"></i>
-                <p className="text-white">MySql - NoSql</p>
+                <p className="text-white">MySQL - NoSQL</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">Sequelize</h5>
+                  <h5 className="text-white text-center">MongoDB</h5>
+                  <h5 className="text-white text-center">Mongoose</h5>
+                  <h5 className="text-white text-center">MySQL Workbench</h5>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="col-md-4 col-sm-6">
-            <div className="card skills-card mt-3">
+            <div className="card btn08 skills-card mt-3">
               <div className="card-body text-center">
                 <i className="fab fa-react fa-5x"></i>
                 <p className="text-white">React JS</p>
+                <div className="ovrly"></div>
+                <div className="hover-text">
+                  <h5 className="text-white text-center">React Hooks</h5>
+                  <h5 className="text-white text-center">The Context API</h5>
+                  <h5 className="text-white text-center">Redux</h5>
+                  <h5 className="text-white text-center">React Router</h5>
+                </div>
               </div>
             </div>
           </div>
