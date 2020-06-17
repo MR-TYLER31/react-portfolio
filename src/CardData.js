8fc7cca1cce3ff2a4724bd3bacc12a8c39b6c732
Tyler Snyder <tylersnyder619406@gmail.com>
2020-06-17 12:55:08 -0600

    added one more project

--- src/CardData.js
+++ src/CardData.js
@@ -1,26 +1,36 @@
 const Projects = [
   {
     id: 1,
+    name: "TLC Medical Transport",
+    image: "/react-portfolio/img/tlc.PNG",
+    description:
+      "TLC serves clients throughout the Southern California area by providing the highest quality and most efficient door-to-door local and long distance non emergency medical transportation",
+    built: "Gatsby JS, React JS, GraphQL, Snip Cart, Contentful CMS",
+    site: "https://tlc-medical-transport.netlify.app/",
+    github: "https://github.com/MR-TYLER31/TLC-Medical-Transport"
+  },
+  {
+    id: 2,
     name: "Trade and Play",
     image: "/react-portfolio/img/tradenplay.PNG",
     description:
-      "Trade and play brings the excitement of sports card collecting onto the web. Collect cards by drafting players.",
+      "Trade and play brings the excitement of sports card collecting onto the web. Collect cards by drafting players. Login as a user and collect cards. Future use will include trading cards with other users",
     built: "React, Node, Express, MongoDB",
     site: "https://trade-and-play.herokuapp.com/",
     github: "https://github.com/Arnette110/Trade-and-Play"
   },
   {
-    id: 2,
+    id: 3,
     name: "Employee Directory",
     image: "/react-portfolio/img/display.PNG",
     description:
-      "Be able to find all your employees with ease. Users can search or sort through the table.",
+      "Be able to find all your employees with ease. Users can search or sort through the table and find information about each employee",
     built: "React, RandomUser.me API",
     site: "https://mr-tyler31.github.io/company-directory/",
     github: "https://github.com/MR-TYLER31/company-directory/tree/master"
   },
   {
-    id: 3,
+    id: 4,
     name: "Recreational Sports League",
     image: "/react-portfolio/img/project2.png",
     description:
@@ -31,7 +41,7 @@ const Projects = [
     github: "https://github.com/seanianking/SoggyBottomboys-Project2"
   },
   {
-    id: 4,
+    id: 5,
     name: "Workout Plan",
     image: "/react-portfolio/img/workout.png",
     description: "As a user you can create a workout plan",
@@ -40,7 +50,7 @@ const Projects = [
     github: "https://github.com/MR-TYLER31/Tylers-Exercise-Tracker"
   },
   {
-    id: 5,
+    id: 6,
     name: "Eat-Da-Burgers",
     image: "/react-portfolio/img/burger.png",
     description: "Add burgers to the menue that you would like to eat",
@@ -49,10 +59,11 @@ const Projects = [
     github: "https://github.com/MR-TYLER31/Thats-A-Tasty-Burger"
   },
   {
-    id: 6,
+    id: 7,
     name: "Event Hub",
     image: "/react-portfolio/img/events.png",
-    description: "Find out when the next events will be",
+    description:
+      "Find out when the next events will be when searching for keyword",
     built:
       "HTML, CSS, Materialize, JavaScript, JQuery, Ticketmaster API, Here API",
     site: "https://tylerphopho.github.io/event-finder/",
@@ -60,7 +71,7 @@ const Projects = [
   },
 
   {
-    id: 7,
+    id: 8,
     name: "Weather App",
     image: "/react-portfolio/img/weather.png",
     description:
